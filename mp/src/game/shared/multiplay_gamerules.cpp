3e12798146e3cc327ffd3e5fcd40d3950bfa384b
Andrew McWatters <andrew@planimeter.org>
2024-12-24 22:37:43 -0700

    Redefine CMultiplayRules::FrameUpdatePostEntityThink()

--- mp/src/game/shared/multiplay_gamerules.cpp
+++ mp/src/game/shared/multiplay_gamerules.cpp
@@ -1938,3 +1938,51 @@ ConVarRef suitcharger( "sk_suitcharger" );
 	}
 
 #endif
+
+#ifndef CLIENT_DLL
+	//=========================================================
+	//=========================================================
+	void CMultiplayRules::FrameUpdatePostEntityThink()
+	{
+		BaseClass::FrameUpdatePostEntityThink();
+
+		float flNow = Plat_FloatTime();
+
+		// Update time when client was last connected
+		if (m_flTimeLastMapChangeOrPlayerWasConnected <= 0.0f)
+		{
+			m_flTimeLastMapChangeOrPlayerWasConnected = flNow;
+		}
+		else
+		{
+			for (int iPlayerIndex = 1; iPlayerIndex <= MAX_PLAYERS; iPlayerIndex++)
+			{
+				player_info_t pi;
+				if (!engine->GetPlayerInfo(iPlayerIndex, &pi))
+					continue;
+#if defined( REPLAY_ENABLED )
+				if (pi.ishltv || pi.isreplay || pi.fakeplayer)
+#else
+				if (pi.ishltv || pi.fakeplayer)
+#endif
+					continue;
+
+				m_flTimeLastMapChangeOrPlayerWasConnected = flNow;
+				break;
+			}
+		}
+
+		// Check if we should cycle the map because we've been empty
+		// for long enough
+		if (mp_mapcycle_empty_timeout_seconds.GetInt() > 0)
+		{
+			int iIdleSeconds = (int)(flNow - m_flTimeLastMapChangeOrPlayerWasConnected);
+			if (iIdleSeconds >= mp_mapcycle_empty_timeout_seconds.GetInt())
+			{
+
+				Log("Server has been empty for %d seconds on this map, cycling map as per mp_mapcycle_empty_timeout_seconds\n", iIdleSeconds);
+				ChangeLevel();
+			}
+		}
+	}
+#endif
