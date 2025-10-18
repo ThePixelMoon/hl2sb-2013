e60fb92121a6e6f04cbc9649ffeebcf02d794196
ThePixelMoon <yourlocalmoon7@gmail.com>
2025-10-18 19:44:06 +0300

    get the code to link properly

---
+++ game/run_hl2sb.bat
@@ -0,0 +1,2 @@
+@echo off
+hl2sb_win64.exe -sw -w 1366 -h 768 -dev -allowdebug -console -condebug +developer 2
---
+++ game/run_hl2sb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+GAME_DEBUGGER=gdb ./hl2sb_linux64 -gl -sw -w 1366 -h 768 -dev -allowdebug -console -condebug +developer 2
