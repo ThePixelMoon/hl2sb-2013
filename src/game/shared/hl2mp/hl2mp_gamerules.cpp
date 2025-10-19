0f208442bde244f187d1ed0637dcc4ecb8655ddd
ThePixelMoon <yourlocalmoon7@gmail.com>
2025-10-19 09:19:12 +0300

    Remove the old HL2MP bots

--- src/game/shared/hl2mp/hl2mp_gamerules.cpp
+++ src/game/shared/hl2mp/hl2mp_gamerules.cpp
@@ -1588,32 +1588,6 @@ CAmmoDef *GetAmmoDef()
 
 #else
 
-#ifdef DEBUG
-
-	// Handler for the "bot" command.
-	void Bot_f()
-	{		
-		// Look at -count.
-		int count = 1;
-		count = clamp( count, 1, 16 );
-
-		int iTeam = TEAM_COMBINE;
-				
-		// Look at -frozen.
-		bool bFrozen = false;
-			
-		// Ok, spawn all the bots.
-		while ( --count >= 0 )
-		{
-			BotPutInServer( bFrozen, iTeam );
-		}
-	}
-
-
-	ConCommand cc_Bot( "bot", Bot_f, "Add a bot.", FCVAR_CHEAT );
-
-#endif
-
 	bool CHL2MPRules::FShouldSwitchWeapon( CBasePlayer *pPlayer, CBaseCombatWeapon *pWeapon )
 	{
 		if ( pPlayer->GetActiveWeapon() && pPlayer->IsNetClient() )
